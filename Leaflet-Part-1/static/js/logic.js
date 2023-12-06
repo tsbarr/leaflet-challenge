e6839a59764ea598ae5ccfa6ec40d67504e3b837
tsbarr <63961884+tsbarr@users.noreply.github.com>
2023-12-06 09:12:54 +0800

    add legend

--- Leaflet-Part-1/static/js/logic.js
+++ Leaflet-Part-1/static/js/logic.js
@@ -72,5 +72,60 @@ function displayMap(data) {
         
     }
 
+    // Create a new choropleth layer.
+    let geojson = L.choropleth(data, {
+
+        // Define which property in the features to use.
+        valueProperty: feature => feature.geometry[2],
+
+        // Set the color scale.
+        scale: ["blue", 'green', 'yellow', 'orange', 'red', 'brown'],
+
+        // The number of breaks in the step range
+        steps: 6,
+
+        // q for quartile, e for equidistant, k for k-means
+        mode: "q",
+        style: {
+            // Border color
+            color: "#fff",
+            weight: 1,
+            fillOpacity: 0.8
+        },
+
+        // Binding a popup to each layer
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(`<h2>${feature.properties.place}</h2><hr><h3>Magnitude: ${feature.properties.mag}</h3><h3>Depth: ${feature.properties.depth}</h3>`);
+        }
+    });
+
+    // Set up the legend.
+    let legend = L.control({ position: "bottomright" });
+    legend.onAdd = function () {
+        let div = L.DomUtil.create("div", "info legend");
+        let limits = geojson.options.limits;
+        let colors = geojson.options.colors;
+        let labels = [];
+
+        // Add the minimum and maximum.
+        let legendInfo = "<h1>Earthquake<br>Depth</h1>" +
+            "<div class=\"labels\">" +
+            "<div class=\"min\">&lt;1......1......5......10......30......100&lt;</div>" +
+            "<div class=\"max\"></div>" +
+            "</div>";
+
+        div.innerHTML = legendInfo;
+
+        limits.forEach(function (limit, index) {
+            labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+        });
+
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
+
+    // Adding the legend to the map
+    legend.addTo(myMap);
+
 
 }
